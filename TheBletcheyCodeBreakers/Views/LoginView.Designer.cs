5d9b1bffe6c378ef2b2ab002ced9c535d2983772
Jabcho_LoL <tedi.truhchev13@abv.bg>
2022-04-20 22:02:29 +0300

    Added login view

---
+++ TheBletcheyCodeBreakers/Views/LoginView.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace TheBletcheyCodeBreakers.Views
+{
+    partial class LoginView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(446, 358);
+            this.txtPass.Multiline = true;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(252, 35);
+            this.txtPass.TabIndex = 1;
+            this.txtPass.Text = "Password";
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.BackColor = System.Drawing.Color.Transparent;
+            this.lblWelcome.Font = new System.Drawing.Font("Modern No. 20", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblWelcome.Location = new System.Drawing.Point(385, 141);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(384, 96);
+            this.lblWelcome.TabIndex = 2;
+            this.lblWelcome.Text = "Login";
+            this.lblWelcome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(446, 303);
+            this.txtUsername.Multiline = true;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(252, 35);
+            this.txtUsername.TabIndex = 3;
+            this.txtUsername.Text = "Username";
+            // 
+            // LoginView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::TheBletcheyCodeBreakers.Properties.Resources.homeBackground;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1144, 640);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.txtPass);
+            this.Name = "LoginView";
+            this.Text = "LoginView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.TextBox txtUsername;
+    }
+}
