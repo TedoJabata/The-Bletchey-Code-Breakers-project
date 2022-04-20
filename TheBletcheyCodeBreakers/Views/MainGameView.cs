13d73c6439a1105377587874fdb9b11d92826970
Jabcho_LoL <tedi.truhchev13@abv.bg>
2022-04-20 22:35:36 +0300

    Added Main Game view and some buttons

---
+++ TheBletcheyCodeBreakers/Views/MainGameView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheBletcheyCodeBreakers.Views
+{
+    public partial class MainGameView : Form
+    {
+        public MainGameView()
+        {
+            InitializeComponent();
+        }
+    }
+}
