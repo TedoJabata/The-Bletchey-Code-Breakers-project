ec9f9f485dfb2b8a498b7b913e35b0639bc149e6
Jabcho_LoL <tedi.truhchev13@abv.bg>
2022-06-23 01:57:19 +0300

    fixes

--- TheBletcheyCodeBreakers/Views/LoginView.Designer.cs
+++ TheBletcheyCodeBreakers/Views/LoginView.Designer.cs
@@ -45,6 +45,7 @@ namespace TheBletcheyCodeBreakers.Views
             this.txtPass.Location = new System.Drawing.Point(446, 389);
             this.txtPass.Multiline = true;
             this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '⚫';
             this.txtPass.Size = new System.Drawing.Size(252, 35);
             this.txtPass.TabIndex = 1;
             // 
